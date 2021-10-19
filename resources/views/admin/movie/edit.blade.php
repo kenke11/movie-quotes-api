6457e7e1495210d3bd64d8dff74851514878fb6d
kenke11 <kenkebashvili2@gmail.com>
2021-10-19 19:35:16 +0400

    style: movie edit page

--- resources/views/admin/movie/edit.blade.php
+++ resources/views/admin/movie/edit.blade.php
@@ -133,6 +133,73 @@
                     Add new quote
                 </button>
 
+                <form action="" class="bg-gray-200 border-gray-500 p-6 rounded-xl my-5 transition duration-1000" style="opacity: 1">
+
+                    <h1 class="text-center font-bold text-xl">Add movie quote</h1>
+
+                    <div class="mb-6 mt-6">
+
+                        <label for="quote_ge" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                            Movie quote in georgian
+                        </label>
+
+                        <input
+                            class="border border-gray-400 p-2 w-full"
+                            type="text"
+                            name="quote_ge"
+                            id="quote_ge"
+                            value="{{$movie->quote_ge}}"
+                            required
+                        >
+
+                        @error('name_ge')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+
+                    </div>
+
+                    <div class="mb-6">
+
+                        <label for="quote_en" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                            Movie quote in english
+                        </label>
+
+                        <input
+                            class="border border-gray-400 p-2 w-full"
+                            type="text"
+                            name="quote_en"
+                            id="quote_en"
+                            value="{{$movie->quote_en}}"
+                            required
+                        >
+
+                        @error('quote_en')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+
+                    </div>
+
+                    <div class="mb-6 mt-6">
+                        <button class="w-full bg-blue-300 hover:bg-blue-500 py-3 rounded-xl transition duration-300">Add quote</button>
+                    </div>
+
+                </form>
+
+
+                <div class="w-full bg-gray-200 border-gray-500 p-6 rounded-xl my-5 transition duration-1000">
+
+                    <form action="" class="mb-3 w-full text-right">
+                        <button class="font-bold text-red-500">X</button>
+                    </form>
+
+                    <h3 class="block mb-2 uppercase font-bold text-lx text-gray-700 mt-3">MOVIE QUOTE IN GEORGIAN</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ex.</p>
+
+                    <h3 class="block mb-2 uppercase font-bold text-lx text-gray-700 mt-5">MOVIE quote IN ENGLISH</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, sed!</p>
+                </div>
+
 
             </div>
 
