e3f9d4a2755d4a62021f98542bfa35da49c41acf
kenke11 <kenkebashvili2@gmail.com>
2022-01-04 13:03:49 +0400

    style: add title and icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/img/logo.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/img/logo.png')}}">
+    <title>Movie quotes</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
